88508c1f07b30866cc1e3ec41c1fd8d38ee34aca
Sara Cristina Ferreira <ferreirasara1501@gmail.com>
2023-01-14 23:12:05 -0300

    fix: remove warnings

--- src/components/Alpaca.tsx
+++ src/components/Alpaca.tsx
@@ -13,14 +13,14 @@ type AlpacaProps = {
 }
 export const Alpaca = ({ accessorieImgLink, backgroundImgLink, earImgLink, eyeImgLink, hairImgLink, legImgLink, mouthImgLink, neckImgLink, noseImgLink }: AlpacaProps) => {
   return <div style={{ position: 'relative' }}>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={backgroundImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={earImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={hairImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={legImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={neckImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={noseImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={accessorieImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={eyeImgLink} style={{ position: 'absolute' }} ></img>
-    <img height={IMG_SIZE} width={IMG_SIZE} src={mouthImgLink} style={{ position: 'absolute' }} ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={backgroundImgLink} style={{ position: 'absolute' }} alt="background" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={earImgLink} style={{ position: 'absolute' }} alt="ear" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={hairImgLink} style={{ position: 'absolute' }} alt="hair" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={legImgLink} style={{ position: 'absolute' }} alt="leg" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={neckImgLink} style={{ position: 'absolute' }} alt="neck" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={noseImgLink} style={{ position: 'absolute' }} alt="nose" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={accessorieImgLink} style={{ position: 'absolute' }} alt="accessorie" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={eyeImgLink} style={{ position: 'absolute' }} alt="eye" ></img>
+    <img height={IMG_SIZE} width={IMG_SIZE} src={mouthImgLink} style={{ position: 'absolute' }} alt="mouth" ></img>
   </div>
 }
